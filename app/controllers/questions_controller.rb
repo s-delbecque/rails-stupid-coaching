42d010083a9c88b3ff2f3759d0f1d0bfada29069
Stephanie Delbecque <stephanie.delbecque@gmail.com>
2019-10-28 15:33:58 +0100

    complete stupid coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,11 +5,11 @@ class QuestionsController < ApplicationController
   def answer
     @questions = params[:question]
     if @questions == "I am going to work"
-    @answer = "Great!"
+      @answer = "Great!"
     elsif @questions.include? "?"
-    @answer = "Silly question, get dressed and go to work!."
+      @answer = "Silly question, get dressed and go to work!."
     else
-    @answer = "I don't care, get dressed and go to work!"
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
